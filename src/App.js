70be53569e02eb5238ded223ff861612a52a58d0
johannagoenagac <johannagoenagac@gmail.com>
2019-05-20 17:49:48 -0300

    se suben carpetas con componentes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      
-       holaaa
-       chauuu
-      </header>
+     
     </div>
   );
 }
