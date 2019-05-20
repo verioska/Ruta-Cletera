5f3582b87abb9effcb8a4081c323833713f2bfee
verioska <rverioska01@gmail.com>
2019-05-20 16:01:29 -0400

    Updates

---
+++ precache-manifest.8d7d3ba5b6bf9d8f9cfa8c90585f2f88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d03e74c748f84892559e99d1d560929",
+    "url": "/Ruta-Cletera/index.html"
+  },
+  {
+    "revision": "d8af0c2759da10e694e5",
+    "url": "/Ruta-Cletera/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Ruta-Cletera/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d8af0c2759da10e694e5",
+    "url": "/Ruta-Cletera/static/js/main.c2a4d614.chunk.js"
+  },
+  {
+    "revision": "abdd14a58981000fb1a8",
+    "url": "/Ruta-Cletera/static/js/runtime~main.ac44f26f.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Ruta-Cletera/static/media/logo.ee7cd8ed.svg"
+  }
+]);
