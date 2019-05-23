f6be48be63aeede13ab70fd3900bcf7d668db005
johannagoenagac <johannagoenagac@gmail.com>
2019-05-23 20:47:31 -0300

    se hacen cambios para mobil más pequeño

--- src/Components/FormSave/FormSave.js
+++ src/Components/FormSave/FormSave.js
@@ -8,31 +8,39 @@ class FormSave extends Component{
         
         return(
             <div className="RouteSave">
+
                 <img className="imgShare" src="https://i.ibb.co/Fb34MFX/routacity.png" alt="routacity" border="0"/>
+
              <div>
+
               <p className="tittle">¿Qué tipo de ruta es?</p>
+
               <div className="input">
                 <input type="radio" name="gender" /> Ruta diaria<br></br>
-                <input type="radio" name="gender" /> Ruta turística<br></br>
+                <input id="seconInput" type="radio" name="gender" /> Ruta turística<br></br>
+            </div>
 
-              </div>
              </div>
+
              <div>
              <p className="tittle">¿Visualisaste alguna de estas características durante tu viaje?</p>
+
              <div className="input2">
              <label class="container">
-                 <input type="checkbox" /> Estacionamiento de Bicicletas
-                 <span class="checkmark"></span>
+                 <input className="checkbox" type="checkbox" /> Estacionamiento de Bicicletas
+                 <span className="checkmark"></span>
              </label>
+
              <label class="container">
-                <input type="checkbox"/> Problemas en la vía
-                <span class="checkmark"></span>
+                <input className="checkbox" type="checkbox"/> Problemas en la vía
+                <span className="checkmark"></span>
             </label>
             </div>
              </div>
+
              <div className="containerBtn">
              <button className="btnCancelar">Cancelar</button>
-             <button onClick={this.props.finalize}className="btnCancelar">Finalizar</button>
+             <button onClick={this.props.finalize} className="btnCancelar">Finalizar</button>
 
              </div>
         </div>
