740f7d591216d6a0f26633c959e76b48d5df64fc
verioska <rverioska01@gmail.com>
2019-05-23 17:07:57 -0400

    se crea screen

---
+++ src/Components/Screen/Screen.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../Screen/Screen.css'
+
+class Screen extends Component{
+    
+    render(){
+       
+        
+        return(
+            <div className="screen" >
+                holissssssssss
+               
+                
+        </div>
+
+        )
+    }
+}
+export default  Screen 
