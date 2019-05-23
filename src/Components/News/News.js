f27e4caefbaaa14f5941883058df5203d31c18e4
johannagoenagac <johannagoenagac@gmail.com>
2019-05-23 13:46:30 -0300

    se agrega componente news con su contenido

--- src/Components/News/News.js
+++ src/Components/News/News.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-// import 'News.css';
+import './News.css';
 
 class News extends Component{
 
     render(){
         return(
             <div>
+              
+                
+                <h5 id="title">Recomendaciones para ti</h5>
+
+                <div id="recomendation">
+
                 <div>
+                    <img className="imgBike" src="https://i.ibb.co/TTgVSXY/img-travel.jpg" alt=""/>
+                </div>
 
-                <h3>Rutas Compartidas</h3>
-
-                <div id="iconRoutesShare">
-                <img src="https://i.ibb.co/rtG0YKS/cycling.png"  alt=""/>
-                <img src="" alt=""/>
-                <img src="" alt=""/>
-                <img src="" alt=""/>
+                <div className="footerImg">
+                    <p className="textLabel">Ruta turistica</p>
+                    <img id="iconTuristic" className="iconImg" src="https://i.ibb.co/GC6zqgG/cycling-4-1.png" alt=""/>
+                    <img className="iconImg" src="https://i.ibb.co/98QLg0Z/parking-1.png" alt=""/>
+                    
                 </div>
 
                 </div>
 
-            </div>
+                <div id="recomendation2">
+
+                <div >
+                    <img className="imgBike" src="https://i.ibb.co/W5bXj7s/arrow-direction-one-way-536.png" alt=""/>
+                </div>
+
+                <div>
+                    <p className="textLabel">Problemas en la via</p>
+                    <img id="iconBrokenRoute"className="iconIMg" src="https://i.ibb.co/rv30Qdv/Group-11.png" alt=""/>
+                    <img className="iconImg" src="https://i.ibb.co/jrj7rdq/map-2.png" alt=""/>
+
+                </div>
+
+                </div>
+
+                </div>
+
+           
         )
     }
 }
